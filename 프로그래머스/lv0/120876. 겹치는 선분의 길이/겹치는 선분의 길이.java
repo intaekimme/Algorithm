c51e188c3fd1d19cc95380744d2d2629d30e5043
InTae Kim <kit938639@gmail.com>
2023-01-02 02:02:55 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -2,6 +2,7 @@ class Solution {
     public int solution(int[][] lines) {
         int answer = 0;
         
+        //  범위 맞춰주기
         for(int i=0; i<3; i++){
             lines[i][0] += 100;
             lines[i][1] += 100;
@@ -10,6 +11,7 @@ class Solution {
         int start = 201;
         int end = 0;
         
+        //  시작점, 끝점 설정
         for(int i=0; i<3; i++){
             if(lines[i][0] < start) start = lines[i][0];
             if(lines[i][1] > end) end = lines[i][1];
@@ -17,12 +19,14 @@ class Solution {
         
         int[] line = new int[201];
         
+        // 구간 표시
         for(int i=0; i<3; i++){
             for(int j = lines[i][0]; j < lines[i][1]; j++){
                 line[j]++;
             }
         }
         
+        //  겹치는 구간은 한 번 이상 체크된 곳
         for(int i=0; i<end; i++){
             if(line[i] > 1) answer++;
         }
