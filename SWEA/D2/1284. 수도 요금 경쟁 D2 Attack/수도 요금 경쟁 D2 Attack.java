40765098c9d96cea849a0a8aa38708cc55357400
InTae Kim <kit938639@gmail.com>
2023-01-05 15:26:09 +0900

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 100 ms, Memory: 18,624 KB -BaekjoonHub

---
+++ SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.lang.Math;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int TC = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for (int tc = 1; tc <= TC; tc++) {
+            st = new StringTokenizer(br.readLine());
+
+            int p = Integer.parseInt(st.nextToken());
+            int q = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+
+            int w = Integer.parseInt(st.nextToken());
+
+            int a = w * p;
+            int b = w > r ? q + (w - r) * s : q;
+
+            sb.append("#").append(tc).append(" ").append(Math.min(a, b)).append("\n");
+        } // end of tc
+
+        System.out.println(sb.toString());
+    }// end of main
+
+}// end of class
