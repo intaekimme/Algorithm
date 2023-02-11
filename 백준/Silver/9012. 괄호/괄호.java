b9a5940183bb727b7a365b5060f669c2450fac6a
InTae Kim <kit938639@gmail.com>
2023-02-11 23:29:01 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int tc = 0; tc < n; tc++) {
+            String arr = br.readLine();
+            Stack<Character> stack = new Stack<>();
+            for (int i = arr.length() - 1; i >= 0; i--) {
+                if(arr.charAt(i) == ')')
+                    stack.push(')');
+                else{
+                    if(stack.isEmpty())
+                        stack.push('(');
+                    else if(stack.peek() == ')')
+                        stack.pop();
+                }
+            }
+            if(stack.size() > 0)
+                System.out.println("NO");
+            else
+                System.out.println("YES");
+        }//    end of tc   
+    }//    end of main
+    
+}//    end of class
